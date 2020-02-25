590a876456ea0c8b42b6aa9a2a004c65b71f8a26
Colton stephens <stephens.colton@icloud.com>
2020-02-25 20:39:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie] [:languages]=[1]
+     programmer_hash[:dennis_ritchie] [:languages]=[
      programmer_hash
 
 end
